5912fe5521f66ec223cc6e50917f0043fb5f03b3
神山大智 <d-kamiyama@starx.co.jp>
2019-08-12 17:14:37 +0900

    今月グループ表示のロジックを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,64 @@
 class UsersController < ApplicationController
-# グループ作成関連  
+# グループ作成関連
   def group_index
-    @group.all 
+    key_list = []
+    name_list = []
+    User.last.pre_group_id.times do |i|
+      user = User.where(pre_group_id: i+1)
+      key_list[i] = user.first.pre_group_id
+      name_list[i] = user.map{|user| user.name}
+    end
+    # @group = {1=>["鵜殿", "小池", "勝又"], 2=>["渡邉", "吉貝", "澤田"]..etc}
+    @group = [key_list, name_list].transpose.to_h
   end
 
-  def shuffle_group
-    @group.all
+  def shuffled_group
+    @shuffled_group = User.all
   end
 
-  
+
 # 社員関連
   def employee_index
-    User.all
-  end  
+    @user = User.all
+  end
 
   def show
-    User.find(params[:id])
-  end 
+    @user = User.find(params[:id])
+  end
 
-  def new 
+  def new
     @user = User.new
   end
 
   def create_user
     @user = User.new(user_params)
     if @user.save!
-      edirect_to users_path, notice: "ユーザー「#{@user.name}を登録しました」"
+      redirect_to users_path, notice: "ユーザー「#{@user.name}」を登録しました"
     else
-      reder :new
+      render :new
     end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update!
+      redirect_to users_path, notice: "ユーザー「#{@user.name}」の情報を更新しました"
+    else
+      render :update
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    redirect_to admin_users_url, notice:  "ユーザー「#{@user.name}を削除しました」"
+    if @user.destroy!
+      redirect_to admin_users_url, notice:  "ユーザー「#{@user.name}」を削除しました"
+    else
+      render :admin
+    end
   end
 
   private
   def user_params
-    params.require(:tuser).permit(:id, :name, :pre_group_id, :group_id, :position_flg, :leader_flg)
+    params.require(:user).permit(:id, :name, :pre_group_id, :group_id, :position_flg, :leader_flg)
   end
 end
